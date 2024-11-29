f031c50f1fa105f0227de1c383e51dc24f0073e0
Ellie Marsden <ejam2@kent.ac.uk>
2024-11-29 09:10:24 +0000

    Added code

--- email.py
+++ email.py
@@ -1,1 +1,37 @@
+import re
 
+def validateemail():
+    email = input("Please enter an email:\n").strip()
+    
+    if re.search(r"^\w+@\w+\.(ac.uk|gov.uk|nhs.net)$", email):
+        if email.endswith("ac.uk"):
+            print("Valid academic email.")
+        elif email.endswith("gov.uk"):
+            print("Valid government email.")
+        else:
+            print("Valid NHS email.")
+    else:
+        listofreasons = reasonforinvalidity(email)
+        print("The lists of reasons that gthe email is invalid:")
+        for i in listofreasons:
+            print(f"-{i}")
+
+def reasonforinvalidity(invalid):
+    listofreasons = []
+    numberofat = 0
+    for i in invalid:
+        if re.search(r"(\w|@|\.)", i) == None and "There is at least one invalid character in the email." not in listofreasons:
+            listofreasons.append("There is at least one invalid character in the email.")
+        elif i == "@" and numberofat > 0 and "There are too many @ symbols in the email." not in listofreasons:
+            listofreasons.append("There are too many @ symbols in the email.")
+            numberofat += 1
+        elif i == "@":
+            numberofat += 1
+    if re.search(r"(ac.uk|gov.uk|nhs.net)$", invalid) == None:
+        listofreasons.append("The ending of the email is invalid.")
+    if len(invalid.split(".")) > 3:
+        listofreasons.append("There are too many dots in the email.")
+    elif len(invalid.split(".")) < 3:
+        listofreasons.append("There are too few dots in the email.")
+    return listofreasons
+            
